9708cc6e3e8e6c11cba4c8d886bd9790a64dc3e5
DestaGull <rioleet@gmail.com>
2017-11-21 16:54:32 +0300

    Initial commit

---
+++ Assets/Scripts/RewardsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardsScript : MonoBehaviour {
+
+    public GameObject rewardPanel;
+    public GameObject videoPanel;
+
+    public Text moneys;
+    int currentCash = 500;
+    int rewardMoney = 50;
+    void Start () {
+        
+    }
+    
+
+    void Update () {
+        
+    }
+    public void ClickStartButton()
+    {
+        rewardPanel.SetActive(true);
+    }
+    public void CloseButtonFunc()
+    {
+        rewardPanel.SetActive(false);
+    }
+    public void RewardedVideoPanelOnClick()
+    {
+        videoPanel.SetActive(true);
+    }
+    public void RewardedVideoClose()
+    {
+        videoPanel.SetActive(false);
+        currentCash += 2 * rewardMoney;
+        moneys.text = currentCash.ToString();
+    }
+}
